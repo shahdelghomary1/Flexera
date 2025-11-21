d5ab0c88c9f99aa265c5aff56e4db696dab16556
shahdelghomary1 <shahdelgomary1@gmail.com>
2025-11-21 19:19:59 +0200

    firstt

--- middleware/multer.js
+++ middleware/multer.js
@@ -2,7 +2,6 @@ import multer from "multer";
 import path from "path";
 import fs from "fs";
 
-// إنشاء مجلد uploads لو مش موجود
 const uploadDir = "uploads";
 if (!fs.existsSync(uploadDir)) fs.mkdirSync(uploadDir);
 
@@ -14,6 +13,7 @@ const storage = multer.diskStorage({
     cb(null, Date.now() + path.extname(file.originalname));
   },
 });
+
 
 const fileFilter = (req, file, cb) => {
   if (file.mimetype.startsWith("image/")) cb(null, true);
